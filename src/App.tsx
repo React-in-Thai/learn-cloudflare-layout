e0f03ddf8e7229a116419e28266abc3e6fb69774
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-04 22:23:28 +0700

    init basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { applyHeaderStyles, Content, EdgeSidebar, Footer, Header, Root } from "./mui-treasury/layout-core-v6"
 
 function App() {
   return (
-    <>
-      Hello
-    </>
+    <Root>
+      <Header sx={{ ...applyHeaderStyles({ fullWidth: true, height: '63px' })}}>
+        Header
+      </Header>
+      <EdgeSidebar>
+        Sidebar
+      </EdgeSidebar>
+      <Content>
+        Content
+      </Content>
+      <Footer>
+        Footer
+      </Footer>
+    </Root>
   )
 }
 
